8e90f3280c0dcb50bc48e2df6eb85188b7b814da
Daniel Vaško <dioforever001@gmail.com>
2023-09-24 00:29:21 +0200

    cabels 1.1 update

--- 1/1/Program.cs
+++ 1/1/Program.cs
@@ -12,60 +12,78 @@
 
             // now we need to go through each test case
             int line = 1;
-            for (int caseNum = 0; caseNum <= t; line++)
+            int correct = 0;
+            for (int caseNumber = 1; caseNumber <= t; caseNumber++)
             {
-                System.Console.WriteLine("Case #" + caseNum + ":" + " line: " + line);
                 // we will take description of each test case and get height, width and how many devices there are 
                 string[] description = input[line].Split(" ");
-                line++;
                 int h = (int.TryParse(description[0], out h) ? h : 0) + 1;
                 int w = (int.TryParse(description[1], out w) ? w : 0) + 1;
                 int n = int.TryParse(description[2], out n) ? n : 0;
                 //  h = height, w = width, n = number of devices
-
-                System.Console.WriteLine("" + t + " " + h + " " + w + " " + n + "alle");
+                System.Console.WriteLine("Case #" + caseNumber + ":" + " line: " + (line + 1));
+                // System.Console.WriteLine(input[line]);
 
 
                 Dictionary<string, int> device_identifiers = new Dictionary<string, int>();
+                Dictionary<string, int> device_count = new Dictionary<string, int>();
                 int device_identifier_count = 1;
                 // now we need to create a grid with the height and width we got from the description
                 int[,] grid = new int[h, w];
                 grid = preset_grid(grid, h, w);
+                string[] devices = new string[n];
 
 
-
-
-                for (int i = 0; i < n; i++)
+                // System.Console.WriteLine(t + " " + h + " " + w + " " + n);
+                for (int i = 0; i <= n; i++)
                 {
-                    // we will take each device and get its height and width
-                    string[] device = input[line + i].Split(" ");
+
+                    string[] device = input[line].Split(" ");
                     int deviceH = int.TryParse(device[0], out deviceH) ? deviceH : 0;
                     int deviceW = int.TryParse(device[1], out deviceW) ? deviceW : 0;
-                    // deviceH = device height, deviceW = device width
+                    // System.Console.WriteLine(deviceH + " " + deviceW + " " + device[2]);
                     if (!device_identifiers.ContainsKey(device[2]))
                     {
                         // we havent seen this device before so we set its identifier
                         device_identifiers.Add(device[2], device_identifier_count);
                         device_identifier_count++;
+                        device_count.Add(device[2], 1);
                     }
+                    // devices[i] = device[0] + " " + device[1] + " " + device[2];
                     grid[deviceH, deviceW] = device_identifiers[device[2]];
-                    System.Console.WriteLine("sole " + deviceH + " " + deviceW + " " + device[2]);
+                    line++;
                 }
-                line += n;
 
-                caseNum++;
-                show_grid(grid);
+
+                if (!even_devices(device_count))
+                {
+                    // System.Console.WriteLine("Devices " + String.Join(" ", devices));
+                    System.Console.WriteLine("ajajaj ");
+                    correct++;
+                    // System.Console.WriteLine(String.Join("\n", devices));
+                    continue;
+                }
+
             }
+            System.Console.WriteLine("Correct: " + correct + " out of " + t);
         }
 
-        public void even_devices()
+        public static bool even_devices(Dictionary<string, int> device_count)
         {
-
+            foreach (KeyValuePair<string, int> device in device_count)
+            {
+                if (device.Value > 2)
+                {
+                    // System.Console.WriteLine("MORE THAN EVEN" + " " + device.Key + " " + device.Value);
+                }
+                if (device.Value % 2 != 0)
+                {
+                    System.Console.WriteLine("NOT EVEN" + " " + device.Key + " " + device.Value);
+                    return false;
+                }
+            }
+            return true;
         }
-
-        // public static int[] taxicab_distances(int n, int[,] grid, int h, int w){
-
-        // }
 
         public static int[,] preset_grid(int[,] grid, int h, int w)
         {
@@ -88,6 +106,11 @@
                 }
                 System.Console.WriteLine();
             }
+        }
+
+        public static void manhatan_distance(int n, int[,] grid, int h, int w)
+        {
+
         }
 
     }
