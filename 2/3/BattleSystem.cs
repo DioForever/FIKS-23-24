8287b841fbee2af4ab6a7d330b01886c3326096b
Daniel Vaško <vaskodaniel1@gmail.com>
2023-10-31 19:06:53 +0100

    bitva 2.3 - Plan Creator - completed

--- 2/3/BattleSystem.cs
+++ 2/3/BattleSystem.cs
@@ -20,14 +20,76 @@ namespace BattleSystem
 
 
 
+    }
+
+    public static class ImmString
+    {
+        public static string sort(string imm)
+        {
+            string immString = "";
+            int immRemLength = 5 - imm.Length;
+            for (int i = 0; i < immRemLength; i++)
+            {
+                if (i == 2)
+                {
+                    immString += " ";
+                    continue;
+                }
+                immString += "0";
+
+            }
+            immString += imm;
+
+            return immString;
+        }
 
     }
+
+    public static class HexToString
+    {
+        public static string sort(string hex)
+        {
+            string hexString = "";
+            int hexRemLength = 8 - hex.Length;
+            for (int i = 0; i < hexRemLength; i++)
+            {
+                hexString += "0";
+
+            }
+            hexString += hex;
+
+            return hexString;
+        }
+
+    }
+
+    public class RegistersPrinter
+    {
+        private Register[] registers;
+        public RegistersPrinter(ref Register reg1, ref Register reg2, ref Register reg3, ref Register reg4, ref Register reg5, ref Register reg6)
+        {
+            Register[] registers = { reg1, reg2, reg3, reg4, reg5, reg6 };
+            this.registers = registers;
+        }
+        public void print()
+        {
+            foreach (Register register in registers)
+            {
+                System.Console.Write($"{register.number} - {register.value} ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+
     public class Register
     {
-        public string value = "0";
-        public Register(string value)
+        public string value = "00 00 00 00";
+        public int number;
+
+        public Register(string value, int number = 0)
         {
-            this.value = "0";
+            this.value = value;
+            this.number = number;
         }
     }
 
@@ -50,7 +112,19 @@ namespace BattleSystem
         {
             return pc;
         }
+
+        public override string ToString()
+        {
+            string opcodeString = opcode.ToString();
+            if (opcode < 10)
+            {
+                opcodeString = "0" + opcodeString;
+            }
+            return $"{opcodeString} {reg1.number}{reg2.number} {ImmString.sort(imm)}";
+        }
     }
+
+
 
     public class NOP : Instruction
     {
@@ -58,7 +132,7 @@ namespace BattleSystem
         private Register reg2;
         private string imm;
 
-        public NOP(Register reg1, Register reg2, string imm, InstructionPlan plan, int pc, int opcode = 1) : base(opcode, reg1, reg2, imm)
+        public NOP(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(69, reg1, reg2, imm)
         {
             this.reg1 = reg1;
             this.reg2 = reg2;
@@ -77,7 +151,7 @@ namespace BattleSystem
         private Register reg1;
         private Register reg2;
         private string imm;
-        public ADD(Register reg1, Register reg2, string imm, InstructionPlan plan, int pc, int opcode = 1) : base(opcode, reg1, reg2, imm)
+        public ADD(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(01, reg1, reg2, imm)
         {
             this.reg1 = reg1;
             this.reg2 = reg2;
@@ -87,12 +161,14 @@ namespace BattleSystem
         }
         public override int Work(int pc)
         {
-            System.Console.WriteLine($"ADD 01 {reg1.value}{reg2.value} 00 0{imm}");
-            int reg1Value = Convert.ToInt32(reg1.value, 16);
-            int reg2Value = Convert.ToInt32(reg2.value, 16);
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"ADD 01 {reg1.number}{reg2.number} {immString}");
+            imm = imm.Replace(" ", "");
+            int reg1Value = Convert.ToInt32(reg1.value.Replace(" ", ""), 16);
+            int reg2Value = Convert.ToInt32(reg2.value.Replace(" ", ""), 16);
             int immValue = Convert.ToInt32(imm, 16);
             string reg1Hex = (reg1Value + reg2Value + immValue).ToString("X");
-            reg1.value = reg1Hex;
+            reg1.value = HexToString.sort(reg1Hex);
             pc++;
             return pc;
         }
@@ -102,7 +178,7 @@ namespace BattleSystem
         private Register reg1;
         private Register reg2;
         private string imm;
-        public SUB(Register reg1, Register reg2, string imm, InstructionPlan plan, int pc, int opcode = 1) : base(opcode, reg1, reg2, imm)
+        public SUB(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(02, reg1, reg2, imm)
         {
             this.reg1 = reg1;
             this.reg2 = reg2;
@@ -112,9 +188,10 @@ namespace BattleSystem
         }
         public override int Work(int pc)
         {
-            System.Console.WriteLine($"SUB 02 {reg1.value}{reg2.value} 00 0{imm}");
-            int reg1Value = Convert.ToInt32(reg1.value, 16);
-            int reg2Value = Convert.ToInt32(reg2.value, 16);
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"SUB 02 {reg1.number}{reg2.number} {immString}");
+            int reg1Value = Convert.ToInt32(reg1.value.Replace(" ", ""), 16);
+            int reg2Value = Convert.ToInt32(reg2.value.Replace(" ", ""), 16);
             int immValue = Convert.ToInt32(imm, 16);
             string reg1Hex = (reg1Value - (reg2Value + immValue)).ToString("X");
             reg1.value = reg1Hex;
@@ -128,20 +205,19 @@ namespace BattleSystem
         private Register reg2;
         private string imm;
 
-        public MUL(Register reg1, Register reg2, string imm, InstructionPlan plan, int pc, int opcode = 1) : base(opcode, reg1, reg2, imm)
+        public MUL(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(03, reg1, reg2, imm)
         {
             this.reg1 = reg1;
             this.reg2 = reg2;
             this.imm = imm;
-            Work(pc);
-
             plan.Add(this);
         }
         public override int Work(int pc)
         {
-            System.Console.WriteLine($"MUL 03 {reg1.value}{reg2.value} 00 0{imm}");
-            int reg1Value = Convert.ToInt32(reg1.value, 16);
-            int reg2Value = Convert.ToInt32(reg2.value, 16);
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"MUL 03 {reg1.number}{reg2.number} {immString}");
+            int reg1Value = Convert.ToInt32(reg1.value.Replace(" ", ""), 16);
+            int reg2Value = Convert.ToInt32(reg2.value.Replace(" ", ""), 16);
             int immValue = Convert.ToInt32(imm, 16);
             string reg1Hex = (reg1Value * (reg2Value + immValue)).ToString("X");
             reg1.value = reg1Hex;
@@ -154,7 +230,7 @@ namespace BattleSystem
         private Register reg1;
         private Register reg2;
         private string imm;
-        public LOAD(Register reg1, Register reg2, string imm, InstructionPlan plan, int pc, int opcode = 1) : base(opcode, reg1, reg2, imm)
+        public LOAD(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(05, reg1, reg2, imm)
         {
             this.reg1 = reg1;
             this.reg2 = reg2;
@@ -163,7 +239,8 @@ namespace BattleSystem
         }
         public override int Work(int pc)
         {
-            System.Console.WriteLine($"LOAD 05 {reg1.value}{reg2.value} 00 0{imm}");
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"LOAD 05 {reg1.number}{reg2.number} {immString}");
 
             pc++;
             return pc;
@@ -174,7 +251,7 @@ namespace BattleSystem
         private Register reg1;
         private Register reg2;
         private string imm;
-        public STORE(Register reg1, Register reg2, string imm, InstructionPlan plan, int pc, int opcode = 1) : base(opcode, reg1, reg2, imm)
+        public STORE(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(06, reg1, reg2, imm)
         {
             this.reg1 = reg1;
             this.reg2 = reg2;
@@ -183,7 +260,8 @@ namespace BattleSystem
         }
         public override int Work(int pc)
         {
-            System.Console.WriteLine($"STORE 06 {reg1.value}{reg2.value} 00 0{imm}");
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"STORE 06 {reg1.number}{reg2.number} {immString}");
 
             pc++;
             return pc;
@@ -194,7 +272,7 @@ namespace BattleSystem
         private Register reg1;
         private Register reg2;
         private string imm;
-        public MOV(Register reg1, Register reg2, string imm, InstructionPlan plan, int pc, int opcode = 1) : base(opcode, reg1, reg2, imm)
+        public MOV(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(07, reg1, reg2, imm)
         {
             this.reg1 = reg1;
             this.reg2 = reg2;
@@ -203,8 +281,9 @@ namespace BattleSystem
         }
         public override int Work(int pc)
         {
-            System.Console.WriteLine($"MOV 07 {reg1.value}{reg2.value} 00 0{imm}");
-            int reg2Value = Convert.ToInt32(reg2.value, 16);
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"MOV 07 {reg1.number}{reg2.number} {immString}");
+            int reg2Value = Convert.ToInt32(reg2.value.Replace(" ", ""), 16);
             int immValue = Convert.ToInt32(imm, 16);
             string reg1Hex = (reg2Value + immValue).ToString("X");
             reg1.value = reg1Hex;
@@ -217,7 +296,7 @@ namespace BattleSystem
         private Register reg1;
         private Register reg2;
         private string imm;
-        public JUMP(Register reg1, Register reg2, string imm, InstructionPlan plan, int pc, int opcode = 1) : base(opcode, reg1, reg2, imm)
+        public JUMP(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(10, reg1, reg2, imm)
         {
             this.reg1 = reg1;
             this.reg2 = reg2;
@@ -226,9 +305,10 @@ namespace BattleSystem
         }
         public override int Work(int pc)
         {
-            System.Console.WriteLine($"JUMP 10 {reg1.value}{reg2.value} 00 0{imm}");
-            int reg1Value = Convert.ToInt32(reg1.value, 16);
-            int reg2Value = Convert.ToInt32(reg2.value, 16);
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"JUMP 10 {reg1.number}{reg2.number} 00 0{immString}");
+            int reg1Value = Convert.ToInt32(reg1.value.Replace(" ", ""), 16);
+            int reg2Value = Convert.ToInt32(reg2.value.Replace(" ", ""), 16);
             int immValue = Convert.ToInt32(imm, 16);
             if (reg1Value == reg2Value)
             {
@@ -246,7 +326,7 @@ namespace BattleSystem
         private Register reg1;
         private Register reg2;
         private string imm;
-        public REVJUMP(Register reg1, Register reg2, string imm, InstructionPlan plan, int pc, int opcode = 1) : base(opcode, reg1, reg2, imm)
+        public REVJUMP(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(11, reg1, reg2, imm)
         {
             this.reg1 = reg1;
             this.reg2 = reg2;
@@ -255,11 +335,42 @@ namespace BattleSystem
         }
         public override int Work(int pc)
         {
-            System.Console.WriteLine($"REVJUMP 11 {reg1.value}{reg2.value} 00 0{imm}");
-            int reg1Value = Convert.ToInt32(reg1.value, 16);
-            int reg2Value = Convert.ToInt32(reg2.value, 16);
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"REVJUMP 11 {reg1.number}{reg2.number} {immString}");
+            int reg1Value = Convert.ToInt32(reg1.value.Replace(" ", ""), 16);
+            int reg2Value = Convert.ToInt32(reg2.value.Replace(" ", ""), 16);
             int immValue = Convert.ToInt32(imm, 16);
             if (reg1Value == reg2Value)
+            {
+                pc -= Convert.ToInt32(imm, 16);
+            }
+            else
+            {
+                pc++;
+            }
+            return pc;
+        }
+    }
+    public class LTJUMP : Instruction
+    {
+        private Register reg1;
+        private Register reg2;
+        private string imm;
+        public LTJUMP(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(12, reg1, reg2, imm)
+        {
+            this.reg1 = reg1;
+            this.reg2 = reg2;
+            this.imm = imm;
+            plan.Add(this);
+        }
+        public override int Work(int pc)
+        {
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"LTJUMP 12 {reg1.number}{reg2.number} {immString}");
+            int reg1Value = Convert.ToInt32(reg1.value.Replace(" ", ""), 16);
+            int reg2Value = Convert.ToInt32(reg2.value.Replace(" ", ""), 16);
+            int immValue = Convert.ToInt32(imm, 16);
+            if (reg1Value < reg2Value)
             {
                 pc += Convert.ToInt32(imm, 16);
             }
@@ -270,9 +381,145 @@ namespace BattleSystem
             return pc;
         }
     }
-
-
-
+    public class REVLTJUMP : Instruction
+    {
+        private Register reg1;
+        private Register reg2;
+        private string imm;
+        public REVLTJUMP(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(13, reg1, reg2, imm)
+        {
+            this.reg1 = reg1;
+            this.reg2 = reg2;
+            this.imm = imm;
+            plan.Add(this);
+        }
+        public override int Work(int pc)
+        {
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"REVLTJUMP 13 {reg1.number}{reg2.number} {immString}");
+            int reg1Value = Convert.ToInt32(reg1.value.Replace(" ", ""), 16);
+            int reg2Value = Convert.ToInt32(reg2.value.Replace(" ", ""), 16);
+            int immValue = Convert.ToInt32(imm, 16);
+            if (reg1Value < reg2Value)
+            {
+                pc -= Convert.ToInt32(imm, 16);
+            }
+            else
+            {
+                pc++;
+            }
+            return pc;
+        }
+    }
+    public class NEQJUMP : Instruction
+    {
+        private Register reg1;
+        private Register reg2;
+        private string imm;
+        public NEQJUMP(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(14, reg1, reg2, imm)
+        {
+            this.reg1 = reg1;
+            this.reg2 = reg2;
+            this.imm = imm;
+            plan.Add(this);
+        }
+        public override int Work(int pc)
+        {
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"NEQJUMP 14 {reg1.number}{reg2.number} {immString}");
+            int reg1Value = Convert.ToInt32(reg1.value.Replace(" ", ""), 16);
+            int reg2Value = Convert.ToInt32(reg2.value.Replace(" ", ""), 16);
+            int immValue = Convert.ToInt32(imm, 16);
+            if (reg1Value != reg2Value)
+            {
+                pc += Convert.ToInt32(imm, 16);
+            }
+            else
+            {
+                pc++;
+            }
+            return pc;
+        }
+    }
+    public class REVNEQJUMP : Instruction
+    {
+        private Register reg1;
+        private Register reg2;
+        private string imm;
+        public REVNEQJUMP(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(15, reg1, reg2, imm)
+        {
+            this.reg1 = reg1;
+            this.reg2 = reg2;
+            this.imm = imm;
+            plan.Add(this);
+        }
+        public override int Work(int pc)
+        {
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"REVNEQJUMP 15 {reg1.number}{reg2.number} {immString}");
+            int reg1Value = Convert.ToInt32(reg1.value.Replace(" ", ""), 16);
+            int reg2Value = Convert.ToInt32(reg2.value.Replace(" ", ""), 16);
+            int immValue = Convert.ToInt32(imm, 16);
+            if (reg1Value != reg2Value)
+            {
+                pc -= Convert.ToInt32(imm, 16);
+            }
+            else
+            {
+                pc++;
+            }
+            return pc;
+        }
+    }
+    public class SETIMMLOW : Instruction
+    {
+        private Register reg1;
+        private Register reg2;
+        private string imm;
+        public SETIMMLOW(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(20, reg1, reg2, imm)
+        {
+            this.reg1 = reg1;
+            this.reg2 = reg2;
+            this.imm = imm;
+            plan.Add(this);
+        }
+        public override int Work(int pc)
+        {
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"SETIMMLOW 20 {reg1.number}{reg2.number} {immString}");
+            imm = imm.Replace(" ", "");
+            // System.Console.WriteLine(reg1.value);
+            // System.Console.WriteLine(HexToString.sort(imm + reg1.value.Substring(4, 4)));
+            reg1.value = HexToString.sort(imm + reg1.value.Substring(4, 4));
+            pc++;
+            return pc;
+        }
+    }
+    public class SETIMMHIGH : Instruction
+    {
+        private Register reg1;
+        private Register reg2;
+        private string imm;
+        public SETIMMHIGH(Register reg1, Register reg2, string imm, InstructionPlan plan) : base(21, reg1, reg2, imm)
+        {
+            this.reg1 = reg1;
+            this.reg2 = reg2;
+            this.imm = imm;
+            plan.Add(this);
+        }
+        public override int Work(int pc)
+        {
+            string immString = ImmString.sort(imm);
+            System.Console.WriteLine($"SETIMMHIGH 21 {reg1.number}{reg2.number} {immString}");
+            imm = imm.Replace(" ", "");
+            // System.Console.WriteLine(reg1.value);
+            // System.Console.WriteLine(HexToString.sort(imm + reg1.value.Substring(4, 4)));
+            string tempRegValue = HexToString.sort(imm + reg1.value.Substring(4, 4));
+            reg1.value = reg1.value.Substring(0, 4) + tempRegValue.Substring(4, 4);
+            pc++;
+            return pc;
+        }
+    }
 
 
     public class InstructionPlan
--- 2/3/Program.cs
+++ 2/3/Program.cs
@@ -7,24 +7,47 @@ namespace Battle
         static void Main(string[] args)
         {
             InstructionPlan plan = new InstructionPlan();
-            Register r1 = new Register("0");
-            Register r2 = new Register("0");
-            Register r3 = new Register("0");
-            Register r4 = new Register("0");
-            Register r5 = new Register("0");
-            Register r6 = new Register("0");
+            Register req1 = new Register("00000000", 0);
+            Register req2 = new Register("00000000", 1);
+            Register req3 = new Register("00000000", 2);
+            Register req4 = new Register("00000000", 3);
+            Register req5 = new Register("00000000", 4);
+            Register req6 = new Register("00000000", 5);
+            RegistersPrinter printer = new RegistersPrinter(ref req1, ref req2, ref req3, ref req4, ref req5, ref req6);
 
-            Instruction i1 = new NOP(r1, r2, "0", plan, 0);
-            Instruction i2 = new ADD(r1, r2, "0", plan, 1);
+            Instruction i3 = new ADD(req1, req2, "01", plan);
+            Instruction i4 = new ADD(req1, req2, "01", plan);
+            Instruction i5 = new SETIMMLOW(req1, req2, "2a", plan);
+            Instruction i6 = new SETIMMHIGH(req1, req2, "4b", plan);
+            Instruction i7 = new REVJUMP(req2, req2, "04", plan);
 
 
+            int counter = 0;
             for (int pc = 0; pc < plan.instructions.Count;)
             {
+                counter++;
                 if (pc >= plan.instructions.Count && pc < 0)
                 {
                     System.Console.WriteLine("Error: pc out of bounds");
                 }
                 pc = plan.instructions[pc].Work(pc);
+                printer.print();
+                if (counter > 50)
+                {
+                    System.Console.WriteLine("Error: infinite loop");
+                    break;
+                }
+            }
+
+            if (true)
+            {
+                // Here we save the plan into a file
+                System.IO.StreamWriter file = new System.IO.StreamWriter("planCode.txt");
+                foreach (Instruction instruction in plan.instructions)
+                {
+                    file.WriteLine(instruction.ToString());
+                }
+                file.Close();
             }
 
 
