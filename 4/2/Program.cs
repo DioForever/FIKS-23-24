5c62e276fda78d73692b0b25967d9a6e005b4e20
Daniel Vaško <vaskodaniel1@gmail.com>
2024-02-18 21:15:21 +0100

    Round 4

---
+++ 4/2/Program.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Graph
+{
+    private Dictionary<int, List<int>> adjacencyList;
+
+    public Graph()
+    {
+        adjacencyList = new Dictionary<int, List<int>>();
+    }
+
+    public void Add(int start, int end)
+    {
+        if (!adjacencyList.ContainsKey(start))
+            adjacencyList[start] = new List<int>();
+
+        if (!adjacencyList.ContainsKey(end))
+            adjacencyList[end] = new List<int>();
+
+        adjacencyList[start].Add(end);
+        adjacencyList[end].Add(start);
+    }
+
+    public List<List<int>> FindShortestPaths(int start, int destination)
+    {
+        List<List<int>> result = new List<List<int>>();
+        Queue<List<int>> queue = new Queue<List<int>>();
+        int shortestPathLength = -1;
+
+        queue.Enqueue(new List<int> { start });
+
+        while (queue.Count > 0)
+        {
+            List<int> path = queue.Dequeue();
+            int node = path.Last();
+
+            if (node == destination)
+            {
+                if (shortestPathLength == -1 || path.Count == shortestPathLength)
+                {
+                    shortestPathLength = path.Count;
+                    result.Add(path);
+                }
+                else
+                {
+                    // All paths with the same length have been found
+                    break;
+                }
+            }
+            else
+            {
+                if (!adjacencyList.ContainsKey(node)) return result;
+                foreach (int neighbor in adjacencyList[node])
+                {
+                    if (!path.Contains(neighbor))
+                    {
+                        List<int> newPath = new List<int>(path);
+                        newPath.Add(neighbor);
+                        queue.Enqueue(newPath);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        var input = ParseInput("input.txt");
+        List<string> output = new List<string>();
+
+        for (int i = 0; i < input.graphs.Length; i++)
+        {
+            System.Console.WriteLine($"New Task {i}/{input.graphs.Length - 1}");
+
+            Graph graph = input.graphs[i];
+            int n = input.ns[i];
+
+            Dictionary<(int, int), (List<int>, List<int>)> edges = input.edgesList[i];
+
+            List<List<int>> shortestPaths = graph.FindShortestPaths(0, n - 1);
+
+            int bestTemp = -100000;
+            bool outcome = false;
+            List<int> shortestPath = new List<int>();
+            foreach (List<int> path in shortestPaths)
+            {
+                List<int> pathFormated;
+                List<(int, int)> options;
+                int tempTotal;
+                (pathFormated, options, tempTotal) = formatPaths(path, edges);
+
+                var (pathOptined, temp) = applyOptions(pathFormated, options, tempTotal);
+                if (Math.Abs(temp) < Math.Abs(bestTemp))
+                {
+                    bestTemp = temp;
+                    shortestPath = pathOptined;
+                    outcome = true;
+                }
+            }
+
+            if (!outcome)
+            {
+                System.Console.WriteLine("ajajaj");
+                output.Add("ajajaj");
+            }
+            else
+            {
+                if (shortestPath.Count == 0) { output.Add("ajajaj"); }
+                else
+                {
+                    System.Console.WriteLine($"pohoda {bestTemp} {shortestPath.Count} {string.Join(" ", shortestPath)}");
+                    output.Add($"pohoda {bestTemp} {shortestPath.Count} {string.Join(" ", shortestPath)}");
+                }
+            }
+            File.WriteAllLines("output.txt", output);
+        }
+    }
+
+    static (List<int> pathOptined, int temp) applyOptions(List<int> path, List<(int, int)> options, int temp)
+    {
+        List<int> pathOptined = new List<int>(path);
+        for (int i = 0; i < options.Count; i++)
+        {
+            if (temp < 2) break;
+            var repPair = options[i];
+            if (pathOptined.Contains(repPair.Item1))
+            {
+                int index = path.FindIndex(a => a == repPair.Item1);
+                pathOptined[index] = repPair.Item2;
+                temp -= 2;
+            }
+            else if (pathOptined.Contains(repPair.Item2))
+            {
+                int index = path.FindIndex(a => a == repPair.Item2);
+                pathOptined[index] = repPair.Item1;
+                temp -= 2;
+
+            }
+        }
+        return (pathOptined, temp);
+    }
+
+    static (List<int> pathFormated, List<(int, int)> options, int tempTotal) formatPaths(List<int> path, Dictionary<(int, int), (List<int> Idents, List<int> Temps)> edges)
+    {
+        List<int> pathFormated = new List<int>();
+        List<(int, int)> options = new List<(int, int)>();
+        int tempTotal = 0;
+        for (int i = 1; i < path.Count; i++)
+        {
+            var key = (path[i - 1], path[i]);
+
+            if (!edges.ContainsKey(key)) key = (path[i], path[i - 1]);
+            List<int> idents = edges[key].Idents;
+            List<int> temps = edges[key].Temps;
+
+            if (idents.Count > 1)
+            {
+                var index = 0;
+                bool found = false;
+                bool addedNegative = false;
+                for (int l = 0; l < idents.Count; l++)
+                {
+                    if (found && !addedNegative && temps[l] == -1)
+                    {
+                        options.Add((idents[index], idents[l]));
+                        addedNegative = true;
+                    }
+                    if (temps[l] == 1 && !found)
+                    {
+                        index = l;
+                        found = true;
+                    }
+                }
+
+                pathFormated.Add(idents[index]);
+                tempTotal += temps[index];
+
+            }
+            else
+            {
+                pathFormated.Add(idents[0]);
+                tempTotal += temps[0];
+            }
+
+        }
+
+        return (pathFormated, options, tempTotal);
+    }
+
+    static (Graph[] graphs, int[] ns, List<Dictionary<(int, int), (List<int>, List<int>)>> edgesList) ParseInput(string path)
+    {
+        string[] input = File.ReadAllLines(path);
+        int tMax = int.Parse(input[0]);
+        List<Graph> graphs = new List<Graph>();
+        List<Dictionary<(int, int), (List<int> idents, List<int> temps)>> edgesList = new List<Dictionary<(int, int), (List<int>, List<int>)>>();
+        List<int> ns = new List<int>();
+        int lineCounter = 1;
+        for (int t = 0; t < tMax; t++)
+        {
+            string[] taskValues = input[lineCounter].Split(" ");
+            System.Console.WriteLine(t + 1 + " - " + (lineCounter + 1));
+            System.Console.WriteLine(string.Join(", ", taskValues));
+            lineCounter++;
+
+            int n = int.Parse(taskValues[0]);
+            int mMax = int.Parse(taskValues[1]);
+
+            Graph graph = new Graph();
+            Dictionary<(int, int), (List<int> Idents, List<int> Temps)> edges = new Dictionary<(int, int), (List<int>, List<int>)>();
+
+
+            for (int m = 0; m < mMax; m++)
+            {
+                string[] edge = input[lineCounter].Split(" ");
+                lineCounter++;
+                int startNode = int.Parse(edge[0]);
+                int endNode = int.Parse(edge[1]);
+
+                graph.Add(startNode, endNode);
+
+                int temperature = edge[2] == "ohniva" ? 1 : -1;
+                if (edges.ContainsKey((startNode, endNode)) || edges.ContainsKey((endNode, startNode)))
+                {
+                    var keyUsed = (startNode, endNode);
+                    if (edges.ContainsKey((endNode, startNode)))
+                    {
+                        keyUsed = (endNode, startNode);
+                    }
+                    List<int> idents = edges[keyUsed].Idents;
+                    List<int> temps = edges[keyUsed].Temps;
+
+                    idents.Add(m);
+                    temps.Add(temperature);
+
+                    edges[keyUsed] = (idents, temps);
+                }
+                else
+                {
+                    List<int> idents = new List<int>();
+                    List<int> temps = new List<int>();
+
+                    idents.Add(m);
+                    temps.Add(temperature);
+
+                    edges[(startNode, endNode)] = (idents, temps);
+                }
+
+            }
+
+            foreach (var key in edges.Keys)
+            {
+                var values = edges[key];
+                var idens = values.Idents;
+                var teps = values.Temps;
+            }
+            graphs.Add(graph);
+            ns.Add(n);
+            edgesList.Add(edges);
+
+        }
+
+
+
+        return (graphs.ToArray(), ns.ToArray(), edgesList);
+    }
+}
---
+++ 4/3 Chess/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+
+class Chess
+{
+    static void Main()
+    {
+        string[] input = File.ReadAllLines("input.txt");
+
+
+        string[] dimensions = input[0].Split();
+        int width = int.Parse(dimensions[0]);
+        int height = int.Parse(dimensions[1]);
+        int enemiesCount = int.Parse(dimensions[2]);
+        string[,] map = new string[height, width];
+
+        string[] frodoCoords = input[1].Split();
+        int frodoX = int.Parse(frodoCoords[0]);
+        int frodoY = int.Parse(frodoCoords[1]);
+        map[frodoX, frodoY] = "F";
+
+        Dictionary<int, Func<int, int, bool>> rules = new Dictionary<int, Func<int, int, bool>>();
+
+        for (int i = 0; i < enemiesCount; i++)
+        {
+            string[] enemyData = input[i + 2].Split();
+            char type = enemyData[0][0];
+            int enemyX = int.Parse(enemyData[1]);
+            int enemyY = int.Parse(enemyData[2]);
+            char direction = enemyData[3][0];
+
+            map[enemyX, enemyY] = type.ToString();
+            var rule = GetRule(type, enemyX, enemyY, direction);
+            rules.Add(i, rule);
+
+            Console.WriteLine($"Type: {type}, Position: ({enemyX}, {enemyY}), Direction: {direction}");
+        }
+
+        PrintMap(map, rules);
+
+
+    }
+
+    public static void PrintMap(string[,] map, Dictionary<int, Func<int, int, bool>> rules)
+    {
+        for (int x = 0; x < map.GetLength(1); x++)
+        {
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                string type = map[y, x];
+                if (type == null) type = "-";
+                if (CheckRules(y, x, rules) == false) type = "X";
+                Console.Write($"{type} ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+    public static bool CheckRules(int x, int y, Dictionary<int, Func<int, int, bool>> rules)
+    {
+        for (int i = 0; i < rules.Keys.Count; i++)
+        {
+            int key = rules.Keys.ToArray()[i];
+
+            if (rules[key](x, y) == false) return false;
+        }
+        return true;
+    }
+
+    public static Func<int, int, bool> GetRule(char type, int ex, int ey, char direction)
+    {
+        switch (type)
+        {
+            case 'Z': // Zaporak
+                return (fx, fy) => CheckZaporak(ex, ey, fx, fy, direction);
+
+            case 'C': // CD Mechanika
+                return (fx, fy) => CheckCDMechanika(ex, ey, fx, fy, direction);
+
+            case 'T': // Trojsky kun
+                return (fx, fy) => CheckTrojskyKun(ex, ey, fx, fy);
+
+            case 'V': // Vysilac
+                return (fx, fy) => CheckVysilac(ex, ey, fx, fy);
+
+            default:
+                throw new ArgumentException("Invalid type");
+        }
+    }
+    public static bool CheckZaporak(int ex, int ey, int fx, int fy, char direction)
+    {
+        if (ex == fx && ey == fy) return true;
+        switch (direction)
+        {
+            case 'J':
+                if ((fx == ex - 1 && fy == ey - 1) || (fx == ex + 1 && fy == ey - 1)) return false;
+                break;
+            case 'S':
+                if ((fx == ex - 1 && fy == ey + 1) || (fx == ex + 1 && fy == ey + 1)) return false;
+                break;
+            case 'V':
+                if ((fx == ex + 1 && fy == ey - 1) || (fx == ex + 1 && fy == ey + 1)) return false;
+                break;
+            case 'Z':
+                if ((fx == ex - 1 && fy == ey - 1) || (fx == ex - 1 && fy == ey + 1)) return false;
+                break;
+            default:
+                throw new ArgumentException("Invalid type");
+        }
+        return true;
+    }
+    public static bool CheckCDMechanika(int ex, int ey, int fx, int fy, char direction)
+    {
+        if (ex == fx && ey == fy) return true;
+        switch (direction)
+        {
+            case 'J':
+                if (fx == ex && fy <= ey) return false;
+                break;
+            case 'S':
+                if (fx == ex && fy >= ey) return false;
+                break;
+            case 'Z':
+                if (fx <= ex && fy == ey) return false;
+                break;
+            case 'V':
+                if (fx >= ex && fy == ey) return false;
+                break;
+            default:
+                throw new ArgumentException("Invalid type");
+        }
+        return true;
+    }
+    public static bool CheckTrojskyKun(int ex, int ey, int fx, int fy)
+    {
+        if (ex == fx && ey == fy) return true;
+
+        if (fy - 2 == ey && fx + 1 == ex) return false;
+        if (fy - 2 == ey && fx - 1 == ex) return false;
+
+        if (fy - 1 == ey && fx + 2 == ex) return false;
+        if (fy - 1 == ey && fx - 2 == ex) return false;
+
+        if (fy + 1 == ey && fx - 2 == ex) return false;
+        if (fy + 1 == ey && fx + 2 == ex) return false;
+
+        if (fy + 2 == ey && fx + 1 == ex) return false;
+        if (fy + 2 == ey && fx - 1 == ex) return false;
+
+        return true;
+    }
+    public static bool CheckVysilac(int ex, int ey, int fx, int fy)
+    {
+        if (ex == fx && ey == fy) return true;
+
+        // int Absfx = Math.Abs(fx);
+        // int Absfy = Math.Abs(fy);
+        int diffX = Math.Abs(ex - fx);
+        int diffY = Math.Abs(ey - fy);
+        if (diffX == diffY) return false;
+        return true;
+    }
+
+}
